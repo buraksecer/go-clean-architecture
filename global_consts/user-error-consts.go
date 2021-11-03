8f39c8a6b5a5d62d3ba5f6b44ac00d312d594bf2
burak seçer <buraksecer@users.noreply.github.com>
2021-11-03 22:19:13 +0300

    u

---
+++ global_consts/user-error-consts.go
@@ -0,0 +1,8 @@
+package global_consts
+
+const (
+	FirstnameCannotBeEmpty string = "firstname cannot be empty"
+	LastnameCannotBeEmpty  string = "lastname cannot be empty"
+	UsernameCannotBeEmpty  string = "username cannot be empty"
+	PasswordCannotBeEmpty  string = "password cannot be empty"
+)
