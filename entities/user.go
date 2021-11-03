39d49ec6ddeae43f22c617e22c827ed3ec1d9ee7
burak seçer <buraksecer@users.noreply.github.com>
2021-11-03 23:26:39 +0300

    Update user.go

--- entities/user.go
+++ entities/user.go
@@ -2,6 +2,7 @@ package entities
 
 import (
 	"errors"
+	"github.com/buraksecer/go-clean-architecture/global_consts"
 )
 
 type User struct {
@@ -14,21 +15,14 @@ type User struct {
 func (input User) Validate() error {
 	switch {
 	case input.FirstName == "":
-		return errors.New(FirstnameCannotBeEmpty)
+		return errors.New(global_consts.FirstnameCannotBeEmpty)
 	case input.LastName == "":
-		return errors.New(LastnameCannotBeEmpty)
+		return errors.New(global_consts.LastnameCannotBeEmpty)
 	case input.Username == "":
-		return errors.New(UsernameCannotBeEmpty)
+		return errors.New(global_consts.UsernameCannotBeEmpty)
 	case input.Password == "":
-		return errors.New(PasswordCannotBeEmpty)
+		return errors.New(global_consts.PasswordCannotBeEmpty)
 	default:
 		return nil
 	}
 }
-
-const (
-	FirstnameCannotBeEmpty string = "firstname cannot be empty"
-	LastnameCannotBeEmpty  string = "lastname cannot be empty"
-	UsernameCannotBeEmpty  string = "username cannot be empty"
-	PasswordCannotBeEmpty  string = "password cannot be empty"
-)
